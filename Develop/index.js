075199d74a8a75bb9b9344c9405a21e7e672ff56
unknown <jtmeyer2014@gmail.com>
2022-11-01 13:14:15 -0400

    added prompt with inquirer questions and tested console.log to see if data is being stored

--- Develop/index.js
+++ Develop/index.js
@@ -2,12 +2,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-console.log("run test");
+// console.log("run test");
 
 // TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of this project?',
+    }, 
+    {
+        type: 'input',
+        name: 'creator',
+        message: 'Who created this project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a brief, but descriptive summary of this project.',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the instructions to install this program?',
+    },
+    {
+        type: 'input',
+        name: 'useage',
+        message: 'Please provide the end user or potential contributor with clear useage information.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Tell us how to contribute to this open-source project.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are the instructions to test this program?',
+    },
+]
+
+inquirer
+    .prompt((questions))
+
+    .then((response) => 
+    console.log(response)
+    );
 
     
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
